06c342b93798f46dcc40ec3e7eadb82e35a9d896
smaibam <smaibam@LT-B3Q04H2.ssc.tsc>
2018-02-16 15:27:46 -0600

    done

--- MyProject/src/JsonParser.java
+++ MyProject/src/JsonParser.java
@@ -4,11 +4,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-
 import org.json.*;
 
 
 public class JsonParser {
+	
+	private Integer Level = 1;
+	private Integer Record = 1;
 
 public void jsonArrayToSet(JSONArray jAry, Set<String> result, String targetKey, String subArrayKey, boolean includeNode){
     try {
@@ -16,6 +18,8 @@ public void jsonArrayToSet(JSONArray jAry, Set<String> result, String targetKey,
             JSONObject jObj = jAry.getJSONObject(i);
             boolean hasSubArray = false;
             JSONArray subArray = null;
+            System.out.println(i+":"+jObj);
+            
             if(jObj.has(subArrayKey)){
                 Object possibleSubArray = jObj.get(subArrayKey);
                 if(possibleSubArray instanceof JSONArray){
@@ -38,9 +42,49 @@ public void jsonArrayToSet(JSONArray jAry, Set<String> result, String targetKey,
     
     
 }
+
+
+
+
+public void jsonParseChildParent(JSONArray jAry,String subArrayKey) {
+	try {
+				
+		for (int i = 0; i < jAry.length(); i++) {
+			
+			JSONObject jObj = jAry.getJSONObject(i);
+            boolean hasSubArray = false;
+            JSONArray subArray = null;
+            Record = i+1;
+            System.out.println("Level:"+Level+",Record:"+Record+",Data :"+jObj);
+            
+            if(jObj.has(subArrayKey)){
+                Object possibleSubArray = jObj.get(subArrayKey);
+                if(possibleSubArray instanceof JSONArray){
+                    hasSubArray = true;
+                    subArray = (JSONArray) possibleSubArray;
+                }
+            }
+            
+            if(hasSubArray){
+            	    Level++;
+                	jsonParseChildParent(subArray,subArrayKey);
+            } 
+		
+		}
+		
+	} catch  (JSONException e){
+        e.printStackTrace();
+    }
+	
+}
+
+
 public String getFileContent () throws FileNotFoundException, IOException 
 {
-	try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\smaibam\\git\\MyProject\\MyProject\\file\\input.txt"))) {
+	
+	String FilePath="C:\\\\Users\\\\smaibam\\\\git\\\\MyProject\\\\MyProject\\\\file\\\\input.txt";
+	//String FilePath=("C:\\Users\\sumen\\git\\MyProject\\file\\input.txt");
+	try(BufferedReader br = new BufferedReader(new FileReader(FilePath))) {
 	    StringBuilder sb = new StringBuilder();
 	    String line = br.readLine();
 
@@ -63,8 +107,9 @@ public static void main( String[] args ) throws FileNotFoundException, IOExcepti
 
 	JSONObject jsonObj = new JSONObject(test.getFileContent());
 	JSONArray jsonArr = jsonObj.getJSONArray("members");
-	test.jsonArrayToSet(jsonArr, result, "firstName", "familyMembers", false);
-	System.out.println(result);
+	//test.jsonArrayToSet(jsonArr, result, "firstName", "familyMembers", false);
+	test.jsonParseChildParent(jsonArr, "familyMembers");
+	//System.out.println(result);
 }
 }
 	
