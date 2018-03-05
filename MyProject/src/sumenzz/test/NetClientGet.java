1032153914e7523b684fbc07b5f6aad2950e0dd0
sumen <sumen@192.168.0.19>
2018-03-04 19:18:33 -0600

    done

--- MyProject/src/sumenzz/test/NetClientGet.java
+++ MyProject/src/sumenzz/test/NetClientGet.java
@@ -10,9 +10,11 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Array;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 public class NetClientGet {
@@ -54,11 +56,11 @@ public class NetClientGet {
 	
 	
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 
 		NetClientGet testclient = 	new NetClientGet ();
 		String[] myStrings = { "One", "Two", "Three" };
-		ExecutorService executor = Executors.newFixedThreadPool(1);
+		ExecutorService executor = Executors.newFixedThreadPool(3);
 		
 		
 		for (String string : myStrings) {
@@ -76,13 +78,17 @@ public class NetClientGet {
 		    
 		};
 		executor.execute(task);
-		
+//		System.out.println("hi");
+//		testclient.postData(string, "http://localhost:8088/send");
 		
 		}
 		
 	
 		executor.shutdown();
-		 
+		executor.awaitTermination(3600,TimeUnit.SECONDS) ;
+		
+		System.out.println("bye");
+		
 		
 		
 		
